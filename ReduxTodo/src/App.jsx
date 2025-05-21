8c4f7ad18e2bd541c80976f8a4d0a43be13c35bc
anikakash <anik15-14210@diu.eud.bd>
2025-05-21 16:55:12 +0600

    add

--- ReduxTodo/src/App.jsx
+++ ReduxTodo/src/App.jsx
@@ -1,8 +1,5 @@
 import Todo from './Components/Todo'
-import TaskForm from './Components/TaskForm'
-import TaskList from './Components/TaskList'
 import TodoCrud from './Components/TodoCrud'
-import EditableTable from './Components/EditableTable'
 
 function App() {
 
@@ -12,7 +9,6 @@ function App() {
       <TaskList/> */}
       <Todo/>
       <TodoCrud/>
-      {/* <EditableTable/> */}
     </>
   )
 }
